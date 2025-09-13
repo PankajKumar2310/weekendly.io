f4d54cd2f9e531204e1af4517d0effd2ae47c51d
PankajKumar2310 <pankajkumarkoli885@gmail.com>
2025-09-14 00:34:24 +0530

    Ui Modification

--- src/api/holidays.js
+++ src/api/holidays.js
@@ -1,4 +1,4 @@
-// api/holidays.js
+
 export async function fetchIndianHolidays(year) {
   const apiKey = process.env.REACT_APP_CALENDARIFIC_API_KEY;
   const url = `https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=IN&year=${year}&type=national`;
